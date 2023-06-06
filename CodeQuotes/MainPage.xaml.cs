234812f37f0e9a28014f3083e5bca648dc5da506
yenilikci <m.melihcelik017@outlook.com>
2023-06-07 01:56:37 +0300

    changing the background of the app dynamically has been added

--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -8,5 +8,35 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	Random random = new Random();
+    private void btnGenerateQuote_Clicked(object sender, EventArgs e)
+    {
+		var startColor =
+			System.Drawing.Color.FromArgb(
+				random.Next(0,256),
+				random.Next(0,256),
+                random.Next(0,256));
+        var endColor =
+            System.Drawing.Color.FromArgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+        var colors = ColorUtility
+                        .ColorControls
+                        .GetColorGradient(startColor, endColor, 6);
+
+        float stopOffset = .0f;
+        var stops = new GradientStopCollection();
+        foreach (var color in colors)
+        {
+            stops.Add(new GradientStop(Color.FromArgb(color.Name), stopOffset));
+            stopOffset += .2f;
+        }
+
+        var gradient = new LinearGradientBrush(stops, new Point(0,0), new Point(1,1));
+
+        background.Background = gradient;
+    }
 }
 
